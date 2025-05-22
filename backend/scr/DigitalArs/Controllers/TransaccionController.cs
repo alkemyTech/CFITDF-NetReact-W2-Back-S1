1d5120eba6856832cb482a6255b97a6afbe80a67
nachengue007 <nachotdf123@gmail.com>
2025-05-22 10:08:39 -0300

    Transacciones

---
+++ backend/scr/DigitalArs/Controllers/TransaccionController.cs
@@ -0,0 +1,87 @@
+﻿using DigitalArs.Dtos;
+using DigitalArs.Interfaces;
+using DigitalArs.Models;
+using Microsoft.AspNetCore.Http.Timeouts;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks.Dataflow;
+
+namespace DigitalArs.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+
+public class TransaccionController : ControllerBase
+{
+    private ITransaccionRepository _transaccionRepository;
+
+    public TransaccionController(ITransaccionRepository transaccionRepository)
+    {
+        _transaccionRepository = transaccionRepository;
+    }
+
+    [HttpGet]
+    public ActionResult<Transaccion[]> GetTransacciones()
+    {
+        return Ok(_transaccionRepository.GetAllTransacciones());
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Transaccion> GetTransaccion(int Id)
+    {
+        if (_transaccionRepository.GetTransaccionById(Id) is Transaccion transaccion)
+        {
+            return Ok(transaccion);
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public ActionResult<Transaccion> CreateTransaccion(CreateTransaccionDtos createTransaccionDto)
+    {
+        var transaccion = new Transaccion()
+        {
+            Id_cuentaorigen = createTransaccionDto.Id_transaccion,
+            Id_cuenta_destino = createTransaccionDto.Id_cuenta_destino,
+            monto = createTransaccionDto.monto,
+            fecha = createTransaccionDto.fecha,
+            tipo = createTransaccionDto.tipo,
+        };
+
+        _transaccionRepository.AddTransaccion(transaccion);
+
+        return CreatedAtAction(nameof(GetTransaccion), new { Id_transaccion = transaccion.Id_transaccion }, transaccion);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult UpdateTransaccion(int id, Transaccion updateTransaccion)
+    {
+        if(_transaccionRepository.GetTransaccionById(id) is Transaccion transaccion)
+        {
+            //transaccion.Id_cuentaorigen = updateTransaccion.Id_cuentaorigen;
+            //transaccion.Id_cuenta_destino = updateTransaccion.Id_cuenta_destino;
+            transaccion.monto = updateTransaccion.monto;
+            //transaccion.fecha = updateTransaccion.fecha;
+            transaccion.tipo = updateTransaccion.tipo;
+
+            _transaccionRepository.UpdateTransaccion(transaccion);
+            return NoContent();
+        }
+
+        return NotFound();
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult RemoveTransaccion(int id)
+    {
+        if (_transaccionRepository.GetTransaccionById(id) is Transaccion transaccion)
+        {
+            _transaccionRepository.RemoveTransaccion(id);
+            return NoContent();
+        }
+
+        return NotFound();
+    }
+}
---
+++ backend/scr/DigitalArs/Repositories/TransaccionRepository.cs
@@ -0,0 +1,48 @@
+﻿using DigitalArs.Data;
+using DigitalArs.Interfaces;
+using DigitalArs.Models;
+
+namespace DigitalArs.Repositories
+{
+    public class TransaccionRepository : ITransaccionRepository
+    {
+        private AppDbContext _dbContext;
+
+        public TransaccionRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Transaccion> GetAllTransacciones()
+        {
+            return _dbContext.Transacciones.ToList();
+        }
+
+        public Transaccion? GetTransaccionById(int id)
+        {
+            return _dbContext.Transacciones.FirstOrDefault(t => t.Id_transaccion == id);
+        }
+
+        public void AddTransaccion(Transaccion transaccion)
+        {
+            _dbContext.Transacciones.Add(transaccion);
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateTransaccion(Transaccion transaccion)
+        {
+            _dbContext.Transacciones.Update(transaccion);
+            _dbContext.SaveChanges();
+        }
+
+        public void RemoveTransaccion(int id)
+        {
+            var transaccion = _dbContext.Transacciones.FirstOrDefault(t => t.Id_transaccion == id);
+            if (transaccion != null)
+            {
+                _dbContext.Transacciones.Remove(transaccion);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+}
