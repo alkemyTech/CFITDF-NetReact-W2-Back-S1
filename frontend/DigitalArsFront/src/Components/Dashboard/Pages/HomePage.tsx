9861b6b5586dc998823a5b2587a74e5cb7dcb0aa
mattp04 <matiaspavon8@gmail.com>
2025-06-02 16:21:24 -0300

    Develop Home page

--- frontend/DigitalArsFront/src/Components/Dashboard/Pages/HomePage.tsx
+++ frontend/DigitalArsFront/src/Components/Dashboard/Pages/HomePage.tsx
@@ -1,7 +1,104 @@
-import { Container, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import { teal } from "@mui/material/colors";
+import { PageContainer } from "@toolpad/core";
+import type { Transaction } from "../../../types";
+import { useState } from "react";
+
+const testTransactions: Transaction[] = [
+  {
+    ID_TRANSACCION: 1,
+    ID_CUENTA_ORIGEN: 1,
+    ID_CUENTA_DESTINO: 2,
+    FECHA: new Date(),
+    MONTO: 10000,
+  },
+  {
+    ID_TRANSACCION: 2,
+    ID_CUENTA_ORIGEN: 1,
+    ID_CUENTA_DESTINO: 2,
+    FECHA: new Date(),
+    MONTO: 5250,
+  },
+  {
+    ID_TRANSACCION: 3,
+    ID_CUENTA_ORIGEN: 1,
+    ID_CUENTA_DESTINO: 2,
+    FECHA: new Date(),
+    MONTO: 8300,
+  },
+];
 
 export default function InicioPage() {
-  return <Container maxWidth="sm">
-    <Typography variant="h1">Home Page!</Typography>
-  </Container>;
+  const balance = 5200.0;
+  const [transacctions, setTransacctions] = useState(testTransactions);
+  return (
+    <PageContainer
+      title="Dashboard"
+      breadcrumbs={[]}
+      maxWidth="md"
+      sx={{ textAlign: "start" }}
+    >
+      <Card sx={{ backgroundColor: teal[600], py: "20px", mb: 5 }}>
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <AccountBalanceWalletIcon
+            sx={{ color: teal[200], fontSize: "6em" }}
+            fontSize="inherit"
+          />
+          <Container sx={{ textAlign: "start" }}>
+            <Typography variant="h5" color="white">
+              Balance Total
+            </Typography>
+            <Typography variant="h2" color="white">
+              {balance.toLocaleString("es-AR", {
+                style: "currency",
+                currency: "ARS",
+              })}
+            </Typography>
+          </Container>
+        </CardContent>
+      </Card>
+      <Typography variant="h4" sx={{mb: 4}}>Transacciones Recientes</Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Fecha</TableCell>
+              <TableCell>Monto</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transacctions.map((row) => (
+              <TableRow key={row.ID_TRANSACCION}>
+                <TableCell>
+                  {row.FECHA.toLocaleDateString("es-AR", {
+                    dateStyle: "short",
+                  })}
+                </TableCell>
+                <TableCell>{row.MONTO}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </PageContainer>
+  );
 }
