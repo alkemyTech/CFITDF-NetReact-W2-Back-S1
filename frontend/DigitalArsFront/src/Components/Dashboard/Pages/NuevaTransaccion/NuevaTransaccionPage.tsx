eea57c8e659f6ee841fb97d0e96e1fac403087fd
mattp04 <matiaspavon8@gmail.com>
2025-06-03 19:51:52 -0300

    fix NuevaTransaccion no se muestra

--- frontend/DigitalArsFront/src/Components/Dashboard/Pages/NuevaTransaccion/NuevaTransaccionPage.tsx
+++ frontend/DigitalArsFront/src/Components/Dashboard/Pages/NuevaTransaccion/NuevaTransaccionPage.tsx
@@ -1,16 +1,14 @@
-import { PageContainer, useActivePage } from '@toolpad/core';
+import { PageContainer, type Breadcrumb } from '@toolpad/core';
 import ConsultaAlias from './ConsultaAlias';
 
 function NuevaTransaccionPage() {
-  const activePage = useActivePage()
-
-  // Mas info sobre rutas dinamicas en el breadcumbs:
-  // https://mui.com/toolpad/core/react-page-container/#dynamic-routes
-  const title = "Nueva Transaccion"
-  const path = "nueva_transaccion"
+  const breadcrumbs: Breadcrumb[] = [
+    {title: "Inicio", path: "/dashboard"},
+    {title: "Nueva Transaccion", path: "/dashboard/nueva_transaccion"}
+  ]
 
   return (
-    <PageContainer title='Nueva Transacción' breadcrumbs={[...activePage!.breadcrumbs, {title, path}]}>
+    <PageContainer title='Nueva Transacción' breadcrumbs={breadcrumbs}>
       <ConsultaAlias></ConsultaAlias>
     </PageContainer>
   );
